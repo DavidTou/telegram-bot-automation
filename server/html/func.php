b0a2d94e89c6baac198f21a41022c1e8c636b0e8
David Tougaw <42074677+DavidTou@users.noreply.github.com>
2019-10-23 18:59:39 -0500

    Update func.php

--- server/html/func.php
+++ server/html/func.php
@@ -3,12 +3,12 @@
 $conn;
 
 function connect($sql) {
-    $conn = mysqli_connect("localhost", "root", "rootmyass", "db");
+    $conn = mysqli_connect("localhost", "root", "root", "db");
     $res = mysqli_query($conn, $sql);
     mysqli_close($conn);
     return $res;
 }
-
+// Sends message
 function sendMessage_PM ($chatID, $messaggio, $token, $parse_mode, $keyboard) {
 
       $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
@@ -49,52 +49,6 @@ function sendMessage($chatID, $messaggio, $token, $keyboard) {
       $result = curl_exec($ch);
       curl_close($ch);
 
-     
-    // INLINE KEYBOARD MARKUP
-//    $user = '{"id": $chatID,"first_name" : "David" }';
-//    $callBackQuery1 = '{"id": "aa1", "from": $user }';
-//
-//    //$cam1= '{"text":"Cam1","callback_data": }';
-//    $cam1 = '{"text":"Cam1"}';
-//    $temp = '{"text":"Temp1"}';
-//    //$in_keyboard='{"inline_keyboard":[["Cam1"],["Temp"]]}';
-//    /*
-//      $file = 'update.txt';
-//      // Open the file to get existing content
-//      $current = file_get_contents($file);
-//     */
-//
-
-//    //https://api.telegram.org/bot132507853:AAGwDn4TFifj5t7GE_34sGDjtgAI5GTy8HA
-//    ///sendMessage?chat_id=66926656&text=TEST&
-//    //reply_markup={%22inline_keyboard%22:[[{%22text%22:%22Button1%22,%22url%22:%22https://www.google.com%22}],[{%22text%22:%22Button2%22,%22url%22:%22https://www.google.com%22}]]}
-//    $a = array('text' => "Train 4 School", 'url' => "http://www.training4school.it");
-//    $b = array('text' => "Straz", 'url' => "http://gph.is/1a2CNzE");
-//    $c = array('text' => "APPLE", 'url' => "http://www.apple.com");
-//    $d = array('text' => "Train 4 School", 'url' => "http://www.training4school.it");
-//    $options = array([$a, $b], [$c,$d]);
-//    $replyMarkup = array('inline_keyboard' => $options);
-//    $encodedMarkup = json_encode($replyMarkup, true);
-//    /* // print to file
-//      $current.= "\n".$encodedMarkup;
-//      file_put_contents($file, $current);
-//     */
-//
-//    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
-//    if ($bool)
-//        $url = $url . "&text=" . urlencode($messaggio) . "&reply_markup=" . urlencode($encodedMarkup);
-//    //$url = $url . "&text=" . urlencode($messaggio)."&reply_markup=".urlencode($keyboard);
-//    else
-//        $url = $url . "&text=" . urlencode($messaggio);
-//    $ch = curl_init();
-//    $optArray = array(
-//        CURLOPT_URL => $url,
-//        CURLOPT_RETURNTRANSFER => true
-//    );
-//    curl_setopt_array($ch, $optArray);
-//    $result = curl_exec($ch);
-//    curl_close($ch);
-}
 
 /* 	EXAMPLE
   $token = "bot<insertTokenHere>";
